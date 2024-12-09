82b5f341a7c6baf9a9c4f0d6dafdb3c5785019cc
EuniceTejedaM <L22550375@chihuahua2.tecnm.mx>
2024-12-09 16:02:38 -0600

    practicas

--- Practicas/js/p4.js
+++ Practicas/js/p4.js
@@ -22,13 +22,13 @@ function calcular_prom() {
 }
 
 let horario = {
-    mat1: "Fundamentos de telecomunicación\n",
-    mat2: "Arquitectura de Computadoras\n",
-    mat3: "Fundamentos de Ingeniería de Software\n",
-    mat4: "Sistemas Operativos\n",
-    mat5: "Programación Web\n",
-    mat6: "Lenguajes y Autómatas\n",
-    mat7: "Taller de Bases de Datos\n",
+    mat1: "Fundamentos de telecomunicación 13-14 \n",
+    mat2: "Arquitectura de Computadoras 14-15 \n",
+    mat3: "Fundamentos de Ingeniería de Software 15-16 \n",
+    mat4: "Sistemas Operativos 16-17 \n",
+    mat5: "Programación Web 17-18 \n",
+    mat6: "Lenguajes y Autómatas 18-19\n",
+    mat7: "Taller de Bases de Datos 19-20\n",
     imprimir: function () {
         return this.mat1 + " " + this.mat2 + " " + this.mat3
             + " " + this.mat4 + " " + this.mat5 + " " + this.mat6 + " " + this.mat7;
